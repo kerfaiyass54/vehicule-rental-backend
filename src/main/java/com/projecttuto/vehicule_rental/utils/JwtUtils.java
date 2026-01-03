bac402305b31225af3a87e9ea5ccb97992bf3a36
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-01-03 15:54:50 +0100

    Added jwtutils

--- src/main/java/com/projecttuto/vehicule_rental/utils/JwtUtils.java
+++ src/main/java/com/projecttuto/vehicule_rental/utils/JwtUtils.java
@@ -1,4 +1,4 @@
-package com.projecttuto.vehicule_rental.configuration;
+package com.projecttuto.vehicule_rental.utils;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
